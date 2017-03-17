b18f45b44c2242066f3f90802f77fc5e90a036b6
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-17 12:49:19 +0000

    pupet lint

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Using strace find out why Apache returning a 500 error.
 exec { 'typo' :
-  path => '/bin/',
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+     path => '/bin/',
+     command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
 }
