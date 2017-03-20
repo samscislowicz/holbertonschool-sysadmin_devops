2e90c7c6b86960f82c6b8ecf8b4e01e9ff297cae
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-20 10:37:21 +0000

    pep8'ed

---
+++ 0x17-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+"""
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    usr_id = sys.argv[1]
+    user = requests.get(url + '/users/' + usr_id).json().get("name")
+    todo = requests.get(url + '/todos?userId=' + usr_id).json()
+
+    with open('{}.csv'.format(usr_id), 'w+' newline='') as f:
+        writer = csv.writer(f, delimiter=' ',
+                            quotechar='"'quoting=csv.QUOTE_ALL)
+        for tasks in todo:
+            writer.writerow([usr_id, user, task.get("completed"),
+                             task.get("title")])
