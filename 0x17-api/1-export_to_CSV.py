33dc57a68df5f1ab44196d39467890ddf723886f
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-20 11:18:59 +0000

    alpha order!

--- 0x17-api/1-export_to_CSV.py
+++ 0x17-api/1-export_to_CSV.py
@@ -2,9 +2,9 @@
 """
 Python script to export data in the CSV format.
 """
+import csv
 import requests
 import sys
-import csv
 
 
 if __name__ == "__main__":
--- 0x17-api/2-export_to_JSON.py
+++ 0x17-api/2-export_to_JSON.py
@@ -2,9 +2,9 @@
 """
 Python script to export data in the json format.
 """
+import json
 import requests
 import sys
-import json
 
 
 if __name__ == "__main__":
