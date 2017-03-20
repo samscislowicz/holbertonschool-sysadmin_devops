af7819c5e71ab507fdbfd3f7738bc423077e16fb
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-20 10:41:23 +0000

    removed space

--- 0x17-api/1-export_to_CSV.py
+++ 0x17-api/1-export_to_CSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+
 """
 import requests
 import sys
@@ -12,9 +13,9 @@ if __name__ == "__main__":
     user = requests.get(url + '/users/' + usr_id).json().get("name")
     todo = requests.get(url + '/todos?userId=' + usr_id).json()
 
-    with open('{}.csv'.format(usr_id), 'w+' newline='') as f:
+    with open('{}.csv'.format(usr_id), 'w+') as f:
         writer = csv.writer(f, delimiter=' ',
-                            quotechar='"'quoting=csv.QUOTE_ALL)
+                            quotechar='"', quoting=csv.QUOTE_ALL)
         for tasks in todo:
             writer.writerow([usr_id, user, task.get("completed"),
                              task.get("title")])
