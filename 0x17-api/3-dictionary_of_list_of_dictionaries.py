5916bf1b68888ece4ec30a5807161a277716d6d2
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-20 11:18:09 +0000

    alpha order!

--- 0x17-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x17-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,9 +2,10 @@
 """
 Python script to export data in the json format.
 """
+import json
 import requests
 import sys
-import json
+
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
