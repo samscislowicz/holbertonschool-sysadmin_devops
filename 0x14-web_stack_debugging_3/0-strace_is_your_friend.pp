f95b764e28ebc5858aaba8f3c917e6db40313e20
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-03-24 23:13:48 -0700

    Update 0-strace_is_your_friend.pp

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Using strace find out why Apache returning a 500 error.
 exec { 'sed' :
-     path => '/usr/bin/:/bin/:/user/sbin/:/sbin/',
+     path => '/bin/',
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
 }
